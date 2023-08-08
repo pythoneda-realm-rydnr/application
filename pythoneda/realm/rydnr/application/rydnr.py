7943d1852a29eabd9097c942dc1a72baf87e6184
rydnr <github@acm-sl.org>
2023-08-08 10:12:54 +0200

    Cleaned up __init__.py files and imports

--- pythoneda/realm/rydnr/application/rydnr.py
+++ pythoneda/realm/rydnr/application/rydnr.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import asyncio
-from pythoneda.application.pythoneda import PythonEDA
+from pythoneda.application import PythonEDA
 
 class Rydnr(PythonEDA):
     """
