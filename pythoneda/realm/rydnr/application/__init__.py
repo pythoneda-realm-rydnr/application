70a97fca6f09f60f62b04058a2d403fdc3b61a68
rydnr <github@acm-sl.org>
2023-08-21 12:05:17 +0200

    Fixed import

--- pythoneda/realm/rydnr/application/__init__.py
+++ pythoneda/realm/rydnr/application/__init__.py
@@ -20,4 +20,4 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-from pythoneda.realm.rydnr.application.rydnr import Rydnr
+from pythoneda.realm.rydnr.application.rydnr_app import RydnrApp
