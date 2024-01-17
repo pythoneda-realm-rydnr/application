379d6afd1e64b92c69a9b021c6e1c2575a155b74
rydnr <github@acm-sl.org>
2024-01-17 19:15:20 +0100

    Added vim and emacs settings

--- pythoneda/realm/rydnr/application/rydnr_app.py
+++ pythoneda/realm/rydnr/application/rydnr_app.py
@@ -52,3 +52,11 @@ class RydnrApp(PythonEDA):
 
 if __name__ == "__main__":
     asyncio.run(RydnrApp.main("pythoneda.realm.rydnr.Rydnr"))
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
